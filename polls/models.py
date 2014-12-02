62efa3e2a4f476c526401f524011d6d0b475a7de
Hal Peterson <vobine@users.noreply.github.com>
2014-12-02 15:54:05 -0800

    Stringify questions and choices

--- polls/models.py
+++ polls/models.py
@@ -5,8 +5,16 @@ class Question (models.Model):
     question_text = models.CharField (max_length=200)
     pub_date = models.DateTimeField ('date published')
 
+    def __unicode__ (self):
+        return u'Question {0:d}: "{1:s}"'.format (self.id,
+                                                  self.question_text)
+
 
 class Choice (models.Model):
     question = models.ForeignKey (Question)
     choice_text = models.CharField (max_length=200)
     votes = models.IntegerField (default=0)
+
+    def __unicode__ (self):
+        return u'Choice {0:d} ({1:d} votes): "{2:s}"'.format (
+            self.id, self.votes, self.choice_text)
