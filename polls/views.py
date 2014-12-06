9a853ed148405acdee8d9bd7cbf891be864b9a3f
Hal Peterson <vobine@users.noreply.github.com>
2014-12-06 12:49:14 -0600

    Include DB output on home page

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
+from polls.models import Question
 
 # Create your views here.
 def index (request):
     """Home page!"""
-    return HttpResponse ("Hello, world, you are at the polls.")
+    qList = Question.objects.order_by ('-pub_date')[:5]
+    output = "Hello, world, you are at the polls.\n"
+    output += ', '.join ([p.question_text for p in qList])
+    return HttpResponse (output)
 
 def detail (request, question_id):
     return HttpResponse (
