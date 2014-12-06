4aa1cae52c551937210989ba484fcb52ccc04b35
Hal Peterson <vobine@users.noreply.github.com>
2014-12-06 10:12:14 -0600

    More view skeletons from tutorial.

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,18 @@ from django.http import HttpResponse
 def index (request):
     """Home page!"""
     return HttpResponse ("Hello, world, you are at the polls.")
+
+def detail (request, question_id):
+    return HttpResponse (
+        "You are looking at question {0:s}.".format (
+            question_id))
+
+def results (request, question_id):
+    return HttpResponse (
+        "You are looking at the results of question {0:s}.".format (
+            question_id))
+
+def vote (request, question_id):
+    return HttpResponse (
+        "You are voting on question {0:s}.".format (
+            question_id))
