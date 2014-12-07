ca942ab5bc97230e5a08fe1ec25a10eae57cc5e7
Hal Peterson <vobine@users.noreply.github.com>
2014-12-06 20:29:41 -0600

    Collect and display votes

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 from polls.models import Question
 
 # Create your views here.
@@ -17,11 +18,23 @@ def detail (request, question_id):
                    {'question': question, } )
 
 def results (request, question_id):
-    return HttpResponse (
-        "You are looking at the results of question {0:s}.".format (
-            question_id))
+    question = get_object_or_404 (Question, pk=question_id)
+    return render (request,
+                   'polls/results.html',
+                   { 'question' : question, } )
 
 def vote (request, question_id):
-    return HttpResponse (
-        "You are voting on question {0:s}.".format (
-            question_id))
+    p = get_object_or_404 (Question, pk=question_id)
+    try:
+        selected = p.choice_set.get (pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render (request,
+                       'polls/detail.html',
+                       { 'question' : p,
+                         'error_message': 'You didn\'t select a choice.',
+                     } )
+    else:
+        selected.votes += 1
+        selected.save ()
+        return HttpResponseRedirect (reverse ('polls:results',
+                                              args=(p.id, )))
