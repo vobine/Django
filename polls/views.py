ff6a24b5d79a8771407abf86cdbe665170d02d68
Hal Peterson <vobine@users.noreply.github.com>
2014-12-06 20:38:25 -0600

    Missed an import

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
-from polls.models import Question
+from polls.models import Question, Choice
 
 # Create your views here.
 def index (request):
