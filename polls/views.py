2cc9c9616086d77e929dc515f597e49281614f31
Hal Peterson <vobine@users.noreply.github.com>
2014-12-06 21:41:37 -0600

    Convert to generic views

--- polls/views.py
+++ polls/views.py
@@ -1,29 +1,29 @@
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from polls.models import Question, Choice
 
 
 # Create your views here.
-def index (request):
+class IndexView (generic.ListView):
     """Home page!"""
-    qList = Question.objects.order_by ('-pub_date')[:5]
-    context = {'qList': qList, }
-    return render (request, "polls/index.html", context)
+    template_name = 'polls/index.html'
+    context_object_name = 'qList'
 
-def detail (request, question_id):
+    def get_queryset (self):
+        """Return the last five published questions."""
+        return Question.objects.order_by ('-pub_date')[:5]
+
+class DetailView (generic.DetailView):
     """Show one question in detail."""
-    question = get_object_or_404 (Question, pk=question_id)
-    return render (request,
-                   'polls/detail.html',
-                   {'question': question, } )
+    model = Question
+    template_name = 'polls/detail.html'
 
-def results (request, question_id):
-    question = get_object_or_404 (Question, pk=question_id)
-    return render (request,
-                   'polls/results.html',
-                   { 'question' : question, } )
+class ResultsView (generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote (request, question_id):
     p = get_object_or_404 (Question, pk=question_id)
