baeafcc630b7a6e8aa5e2164172a568a2bf909e0
Hal Peterson <vobine@users.noreply.github.com>
2014-12-08 17:32:59 -0600

    Do not show details of unpublished questions in DB.

--- polls/views.py
+++ polls/views.py
@@ -24,6 +24,10 @@ class DetailView (generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset (self):
+        """Exclude any questions that are not yet published."""
+        return Question.objects.filter (pub_date__lte = timezone.now ())
+
 class ResultsView (generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
